e15898606874e4a4c4b1bb0dc2781a67632b8708
Okeybukks <achebepeter94@gmail.com>
2024-01-23 00:53:53 +0000

    add dynamic variable substitution

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import queryString from 'query-string'
-// import { REACT_APP_BACKEND_IP } from '../config';
+import { BACKEND_IP } from '../AppConfig';
 
-// const baseUrl = REACT_APP_BACKEND_IP+'/api/v1/'
+const baseUrl = BACKEND_IP+'/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
-// console.log(baseUrl)
-const baseUrl = null
+console.log(baseUrl)
+
 const axiosClient = axios.create({
   baseURL: baseUrl,
   paramsSerializer: params => queryString.stringify({ params })
